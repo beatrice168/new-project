97116841d081dea86c42f17e7c070a071f26fd2d
beatrice <helgapaul389@gmail.com>
2023-07-13 11:23:38 +0300

    made some changes

--- bitrick/src/components/MusicList.js
+++ bitrick/src/components/MusicList.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
 
 function MusicList() {
-  const [data, setData] = useState([]);
-
+  const [data,setData] = useState([]);
   useEffect(() => {
     fetch("http://127.0.0.1:5555/music")
       .then(r => r.json())
-      .then((data) => setData(data));
+      .then(data=> {setData(data)});
   }, []);
   return (
     <div></div>
