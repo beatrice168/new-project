6ec5c6719f73c805e9d0a35aa860107330d45942
ErickMwangi <rickthetri20@gmail.com>
2023-07-14 01:17:09 +0300

    initial commit

--- bitrick/src/components/MusiqAlat.js
+++ bitrick/src/components/MusiqAlat.js
@@ -3,10 +3,10 @@ import React from 'react'
 function MusiqAlat({ id,image,audio,artist,created_at,genre}) {
 
   return (
-    <div>
+    <div className = "alert">
         <p>{image}</p>
         <p>{audio}</p>
-        <p>{artist}</p>
+        <p>artist:{artist}</p>
         <p>{created_at}</p>
         <p>{genre}</p>
        
