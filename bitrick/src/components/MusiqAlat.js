f38b6c79ecaa9bf4839ffc4007a46f39dde67ab3
ErickMwangi <rickthetri20@gmail.com>
2023-07-14 02:26:16 +0300

    made some changes

--- bitrick/src/components/MusiqAlat.js
+++ bitrick/src/components/MusiqAlat.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-function MusiqAlat({ id,image,audio,artist,created_at,genre}) {
-
+function MusiqAlat({ id, image, audio, artist, created_at, genre }) {
   return (
-    <div className = "alert">
-        <p>{image}</p>
-        <p>{audio}</p>
-        <p>artist:{artist}</p>
-        <p>{created_at}</p>
-        <p>{genre}</p>
-       
+    <div className="alert">
+      <img src={image} alt="Album Art" />
+      <p> <video controls>
+        <source src={audio} type="video/mp4" />
+        {/* Your browser does not support the video element. */}
+      </video></p>
+      <p>artist: {artist}</p>
+      <p>{created_at}</p>
+      <p>{genre}</p>
     </div>
-  )
+  );
 }
-export default MusiqAlat
+
+export default MusiqAlat;
